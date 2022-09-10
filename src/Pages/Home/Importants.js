f528a9a1df96ce08bae18356df7fbed44f4344d1
humayra akter <humayraakter25@gmail.com>
2022-09-10 11:59:41 +0600

    routes Added

---
+++ src/Pages/Home/Importants.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Importants = () => {
+        return (
+                <div>
+                        
+                </div>
+        );
+};
+
+export default Importants;
